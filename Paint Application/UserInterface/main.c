f5928712bebe50a90a35b0025ea74a56b4e7e1bc
Aniket2107 <had096705@gmail.com>
2022-03-06 22:37:30 +0530

    feat: ui

--- Paint Application/UserInterface/main.c
+++ Paint Application/UserInterface/main.c
@@ -5,105 +5,23 @@
 
 
 PFbyte windowID, canvasID, widget1ID, widget2ID, widget3ID, widget4ID, widget5ID, widget6ID;
-PFdword x,y,a,b,val=10;
+PFdword x,y,a,b,c,d,val=10;
 int idx=0;
 
-// Function to find square root of a PFdword type variable
-PFdword sqroot(PFdword r) {
 
-    if (r==0 || r==1) return r;
- 
-    PFdword start=1, end=r/2 , mid, res, sqr;
 
-    while (start<=end) {
-        mid=(start + end)/2;
-        sqr=mid*mid;
+PFdword sqroot(PFdword r);      // Function to find square root of a PFdword type variable
+void freeHandBtnEventHandler(void);
+void lineBtnEventHandler(void);
+void circleBtnEventHandler(void);
+void rectangleBtnEventHandler(void);
+void saveBtnEventHandler(void);
+void homeScreen(void);
+void clearscreenBtnEventHandler(void);
+void canvasEventHandler(void);
 
-        if (sqr==r) return mid;
 
-        if (sqr<r) {
-            start=mid + 1;
-            res=mid;
-        }
 
-        else end=mid - 1;
-    }
-    return res;
-}
-
-void freeHandBtnEventHandler(void) {
-    idx=1;
-}
-    
-void lineBtnEventHandler(void) {
-    idx=2;
-}
-
-void circleBtnEventHandler(void) {
-    idx=3;
-}
-    
-void rectangleBtnEventHandler(void) {
-    idx=4;
-}
-
-void saveBtnEventHandler(void) {
-    gfxDrawString(val,50,"SAVED",enGfxFont_8X16,BLACK,WHITE);
-    // val+=50;
-}
-    
-void homeScreen(void) {
-    setWindow(windowID);
-    gfxDrawLine(42,36,72,4);
-    gfxDrawCircle(95,20,16);
-    gfxDrawRectangle(118,4,148,36);	
-    gfxDrawString(197,12,"SAVE",enGfxFont_8X16,BLACK,WHITE);
-} 
-
-void clearscreenBtnEventHandler(void) {
-	homeScreen();
-    // val=10;
-	while(1) {
-		if(touchAvailable(&x,&y)==enBooleanTrue) windowEventHandler(windowID,x,y);
-	}
-}
-
-void canvasEventHandler(void) {
-/* the edge cases such as handling of shapes and pixels are not drawn on the buttons are commented here because we wanted to 
-   see about the problems but we will use them in the final build and please have the look at the edge cases as well whether 
-   if they are correct.
-*/
-    switch(idx) 
-    {
-        case 1:
-            gfxDrawPixels(x,y);
-	    	while(touchAvailable(&a,&b)==enBooleanTrue) {
-                // if(b>=42) {}
-                gfxDrawPixels(a,b);
-            }
-            break;
-        case 2:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a || y!=b) && b>=42) {}
-			gfxDrawLine(x,y,a,b);
-            break;
-        case 3:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a || y!=b) && b>=42) {}
-            PFdword radius,tmp=((a-x)*(a-x))+((b-y)*(b-y));
-	    	radius=sqroot(tmp);
-            // if(y-radius>=42 && y+radius<=319 && x-radius>=0 && x+radius>=239) {}
-		    gfxDrawCircle(x,y,radius);
-            break;
-        case 4:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a && y!=b) && b>=42) {}
-			gfxDrawRectangle(x,y,a,b);
-            break;
-        default :
-            break;
-    }
-}
 
 
 static WindowCfg window1= 
@@ -238,6 +156,7 @@ int main()
 	appInit();						
 	gameEngineInit();	
     gfxSetColor(BLACK);	
+    gfxSetPenSize(2);
 	
     // creating widgets
 	createWindow(&windowID,&window1);
@@ -254,9 +173,113 @@ int main()
 
 	while(1) {
 		if(touchAvailable(&x,&y) == enBooleanTrue) {
+            c=x;
+            d=y;
 			windowEventHandler(windowID, x, y);	
 		}
 	}
 
     return 0;
+}
+
+
+
+
+
+PFdword sqroot(PFdword r) {
+
+    if (r==0 || r==1) return r;
+ 
+    PFdword start=1, end=r/2 , mid, res, sqr;
+
+    while (start<=end) {
+        
+        mid=(start + end)/2;
+        sqr=mid*mid;
+
+        if (sqr==r) {
+            return mid;
+        }
+
+        if (sqr<r) {
+            start=mid + 1;
+            res=mid;
+        }
+
+        else {
+            end=mid - 1;
+        }
+    }
+    return res;
+}
+
+void freeHandBtnEventHandler(void) {
+    idx=1;
+}
+    
+void lineBtnEventHandler(void) {
+    idx=2;
+}
+
+void circleBtnEventHandler(void) {
+    idx=3;
+}
+    
+void rectangleBtnEventHandler(void) {
+    idx=4;
+}
+
+void saveBtnEventHandler(void) {
+    gfxDrawString(val,50,"SAVED",enGfxFont_8X16,BLACK,WHITE);
+    // val+=50;
+}
+    
+void homeScreen(void) {
+    setWindow(windowID);
+    gfxDrawLine(42,36,72,4);
+    gfxDrawCircle(95,20,16);
+    gfxDrawRectangle(118,4,148,36);	
+    gfxDrawString(197,12,"SAVE",enGfxFont_8X16,BLACK,WHITE);
+} 
+
+void clearscreenBtnEventHandler(void) {
+	// gfxFillArea(2,42,238,318,WHITE);
+    homeScreen();
+}
+
+void canvasEventHandler(void) {
+/* the edge cases such as handling of shapes and pixels not to be drawn on the buttons are commented here because we wanted to 
+   see about the problems but we will use them in the final build and please have the look at the edge cases as well whether 
+   if they are correct.
+*/
+    switch(idx) 
+    {
+        case 1:
+            gfxDrawPixels(x,y);
+	    	while(touchAvailable(&a,&b)==enBooleanTrue) {
+                // if(b>=42) {}
+                gfxDrawPixels(a,b);
+            }
+            break;
+        case 2:
+            while(touchAvailable(&a,&b)==enBooleanTrue) {}
+            // if((x!=a || y!=b) && b>=42) {}
+			gfxDrawLine(c,d,a,b);
+            break;
+        case 3:
+            while(touchAvailable(&a,&b)==enBooleanTrue) {}
+            // if((x!=a || y!=b) && b>=42) {}
+            PFdword radius,tmp=((a-c)*(a-c))+((b-d)*(b-d));
+	    	radius=sqroot(tmp);
+            // if(y-radius>=42 && y+radius<=319 && x-radius>=0 && x+radius>=239) {}
+		    gfxDrawCircle(c,d,radius);
+            break;
+        case 4:
+            while(touchAvailable(&a,&b)==enBooleanTrue) {}
+            // if((x!=a && y!=b) && b>=42) {}
+			gfxDrawRectangle(c,d,a,b);
+            break;
+        default :
+            break;
+    }
 }
