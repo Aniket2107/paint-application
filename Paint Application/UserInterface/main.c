832717cc61fd2e2400067b4864901ebab2a06a82
Aniket2107 <had096705@gmail.com>
2022-03-15 20:01:19 +0530

    first commit

--- Paint Application/UserInterface/main.c
+++ Paint Application/UserInterface/main.c
@@ -2,15 +2,12 @@
 #include "app.h"
 #include "gameEngine.h"
 
-
-
 PFbyte windowID, canvasID, widget1ID, widget2ID, widget3ID, widget4ID, widget5ID, widget6ID;
-PFdword x,y,a,b,c,d,val=10;
-int idx=0;
+PFdword i, j, a1, b1, a2, b2;
+char shape = 'f';
+int cnt = 0;
 
-
-
-PFdword sqroot(PFdword r);      // Function to find square root of a PFdword type variable
+PFdword sqroot(PFdword r); // Function to find square root of a PFdword type variable
 void freeHandBtnEventHandler(void);
 void lineBtnEventHandler(void);
 void circleBtnEventHandler(void);
@@ -20,266 +17,273 @@ void homeScreen(void);
 void clearscreenBtnEventHandler(void);
 void canvasEventHandler(void);
 
-
-
-
-
-static WindowCfg window1= 
-{
+static WindowCfg window1 =
     {
-        "Window 1",
-        {0,0},
-        {240,320},
-        WHITE,
-        enBooleanFalse
-    },
-    NULL
-};
+        {"Window 1",
+         {0, 0},
+         {240, 320},
+         WHITE,
+         enBooleanFalse},
+        NULL};
 
-static CanvasCfg Canvas=
-{
+static CanvasCfg Canvas =
     {
-        "Canvas",
-        {0,40},
-        {240,280},
+        {"Canvas",
+         {0, 43},
+         {240, 280},
+         WHITE,
+         enBooleanTrue},
+        canvasEventHandler,
+        "",
+        enGfxFont_8X16,
         WHITE,
-        enBooleanTrue
-    },
-    canvasEventHandler,
-    "",
-    enGfxFont_8X16,
-    WHITE,
-    0
-};
+        0};
 
-static WidgetCfg freeHandBtnWidget=
-{
+static WidgetCfg freeHandBtnWidget =
     {
-        "Freehand button widget",
-        {0,0},
-        {38,40},
-        WHITE,
-        enBooleanTrue
-    },
-    freeHandBtnEventHandler,
-    "  FH",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
+        {"Freehand button widget",
+         {0, 0},
+         {40, 43},
+         WHITE,
+         enBooleanTrue},
+        freeHandBtnEventHandler,
+        "  FH",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
-static WidgetCfg lineBtnWidget=
-{
+static WidgetCfg lineBtnWidget =
     {
-        "Line button widget",
-        {38,0},
-        {38,40},
-        WHITE,
-        enBooleanTrue
-    },
-    lineBtnEventHandler,
-    "",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
+        {"Line button widget",
+         {40, 0},
+         {40, 43},
+         WHITE,
+         enBooleanTrue},
+        lineBtnEventHandler,
+        "",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
-static WidgetCfg circleBtnWidget=
-{
+static WidgetCfg circleBtnWidget =
     {
-        "Circle button widget",
-        {76,0},
-        {38,40},
-        WHITE,
-        enBooleanTrue
-    },
-    circleBtnEventHandler,
-    "",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
+        {"Circle button widget",
+         {80, 0},
+         {40, 43},
+         WHITE,
+         enBooleanTrue},
+        circleBtnEventHandler,
+        "",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
-static WidgetCfg rectangleBtnWidget=
-{
+static WidgetCfg rectangleBtnWidget =
     {
-        "Rectangle button widget",
-        {114,0},
-        {38,40},
-        WHITE,
-        enBooleanTrue
-    },
-    rectangleBtnEventHandler,
-    "",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
+        {"Rectangle button widget",
+         {120, 0},
+         {40, 43},
+         WHITE,
+         enBooleanTrue},
+        rectangleBtnEventHandler,
+        "",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
-
-static WidgetCfg clearscreenBtnWidget=
-{
+static WidgetCfg clearscreenBtnWidget =
     {
-        "Clearscreen button widget",
-        {152,0},
-        {38,40},
-        WHITE,
-        enBooleanTrue
-    },
-    clearscreenBtnEventHandler,
-    " CLS",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
+        {"Clearscreen button widget",
+         {160, 0},
+         {36, 43},
+         WHITE,
+         enBooleanTrue},
+        clearscreenBtnEventHandler,
+        " CLS",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
-static WidgetCfg saveBtnWidget=
-{
+static WidgetCfg saveBtnWidget =
     {
-        "Save button widget",
-        {192,0},
-        {48,40},
-        WHITE,
-        enBooleanTrue
-    },
-    saveBtnEventHandler,
-    "",
-    enGfxFont_8X16,
-    BLACK,
-    0
-};
-
+        {"Save button widget",
+         {196, 0},
+         {44, 43},
+         WHITE,
+         enBooleanTrue},
+        saveBtnEventHandler,
+        "",
+        enGfxFont_8X16,
+        BLACK,
+        0};
 
 int main()
 {
 
-	appInit();						
-	gameEngineInit();	
-    gfxSetColor(BLACK);	
+    appInit();
+    gameEngineInit();
+    gfxSetColor(BLACK);
     gfxSetPenSize(2);
-	
+
     // creating widgets
-	createWindow(&windowID,&window1);
-	createCanvas(windowID,&canvasID,&Canvas);
-	createWidget(windowID,&widget1ID,&freeHandBtnWidget);
-	createWidget(windowID,&widget2ID,&lineBtnWidget);
-    createWidget(windowID,&widget3ID,&circleBtnWidget);
-    createWidget(windowID,&widget4ID,&rectangleBtnWidget);
-	createWidget(windowID,&widget5ID,&clearscreenBtnWidget);
-    createWidget(windowID,&widget6ID,&saveBtnWidget);
+    createWindow(&windowID, &window1);
+    createCanvas(windowID, &canvasID, &Canvas);
+    createWidget(windowID, &widget1ID, &freeHandBtnWidget);
+    createWidget(windowID, &widget2ID, &lineBtnWidget);
+    createWidget(windowID, &widget3ID, &circleBtnWidget);
+    createWidget(windowID, &widget4ID, &rectangleBtnWidget);
+    createWidget(windowID, &widget5ID, &clearscreenBtnWidget);
+    createWidget(windowID, &widget6ID, &saveBtnWidget);
 
+    homeScreen();
 
-	homeScreen();							
-
-	while(1) {
-		if(touchAvailable(&x,&y) == enBooleanTrue) {
-            c=x;
-            d=y;
-			windowEventHandler(windowID, x, y);	
-		}
-	}
-
+    while (1)
+    {
+        if (touchAvailable(&i, &j) == enBooleanTrue)
+        {
+            windowEventHandler(windowID, i, j);
+        }
+    }
     return 0;
 }
 
+PFdword sqroot(PFdword r)
+{
 
+    if (r == 0 || r == 1)
+        return r;
 
+    PFdword start = 1, end = r / 2, mid, res, sqr;
 
+    while (start <= end)
+    {
 
-PFdword sqroot(PFdword r) {
+        mid = (start + end) / 2;
+        sqr = mid * mid;
 
-    if (r==0 || r==1) return r;
- 
-    PFdword start=1, end=r/2 , mid, res, sqr;
-
-    while (start<=end) {
-        
-        mid=(start + end)/2;
-        sqr=mid*mid;
-
-        if (sqr==r) {
+        if (sqr == r)
+        {
             return mid;
         }
 
-        if (sqr<r) {
-            start=mid + 1;
-            res=mid;
+        if (sqr < r)
+        {
+            start = mid + 1;
+            res = mid;
         }
 
-        else {
-            end=mid - 1;
+        else
+        {
+            end = mid - 1;
         }
     }
     return res;
 }
 
-void freeHandBtnEventHandler(void) {
-    idx=1;
-}
-    
-void lineBtnEventHandler(void) {
-    idx=2;
+void freeHandBtnEventHandler(void)
+{
+    shape = 'f';
 }
 
-void circleBtnEventHandler(void) {
-    idx=3;
-}
-    
-void rectangleBtnEventHandler(void) {
-    idx=4;
+void lineBtnEventHandler(void)
+{
+    shape = 'l';
 }
 
-void saveBtnEventHandler(void) {
-    gfxDrawString(val,50,"SAVED",enGfxFont_8X16,BLACK,WHITE);
-    // val+=50;
+void circleBtnEventHandler(void)
+{
+    shape = 'c';
 }
-    
-void homeScreen(void) {
+
+void rectangleBtnEventHandler(void)
+{
+    shape = 'r';
+}
+
+void saveBtnEventHandler(void)
+{
+    gfxDrawString(10, 54, "SAVED", enGfxFont_8X16, BLACK, WHITE);
+}
+
+void homeScreen(void)
+{
     setWindow(windowID);
-    gfxDrawLine(42,36,72,4);
-    gfxDrawCircle(95,20,16);
-    gfxDrawRectangle(118,4,148,36);	
-    gfxDrawString(197,12,"SAVE",enGfxFont_8X16,BLACK,WHITE);
-} 
-
-void clearscreenBtnEventHandler(void) {
-	// gfxFillArea(2,42,238,318,WHITE);
-    homeScreen();
+    gfxDrawLine(76, 5, 44, 39);
+    gfxDrawCircle(100, 21, 17);
+    gfxDrawRectangle(124, 4, 156, 39);
+    gfxDrawString(201, 14, "SAVE", enGfxFont_8X16, BLACK, WHITE);
 }
 
-void canvasEventHandler(void) {
-/* the edge cases such as handling of shapes and pixels not to be drawn on the buttons are commented here because we wanted to 
-   see about the problems but we will use them in the final build and please have the look at the edge cases as well whether 
-   if they are correct.
-*/
-    switch(idx) 
+void clearscreenBtnEventHandler(void)
+{
+    gfxFillArea(2, 45, 238, 318, WHITE);
+}
+
+void canvasEventHandler(void)
+{
+
+    switch (shape)
     {
-        case 1:
-            gfxDrawPixels(x,y);
-	    	while(touchAvailable(&a,&b)==enBooleanTrue) {
-                // if(b>=42) {}
-                gfxDrawPixels(a,b);
+    case 'f':
+        gfxDrawPixels(i, j);
+        while (touchAvailable(&a1, &b1) == enBooleanTrue)
+        {
+            if (b1 >= 45)
+            {
+                gfxDrawPixels(a1, b1);
             }
-            break;
-        case 2:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a || y!=b) && b>=42) {}
-			gfxDrawLine(c,d,a,b);
-            break;
-        case 3:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a || y!=b) && b>=42) {}
-            PFdword radius,tmp=((a-c)*(a-c))+((b-d)*(b-d));
-	    	radius=sqroot(tmp);
-            // if(y-radius>=42 && y+radius<=319 && x-radius>=0 && x+radius>=239) {}
-		    gfxDrawCircle(c,d,radius);
-            break;
-        case 4:
-            while(touchAvailable(&a,&b)==enBooleanTrue) {}
-            // if((x!=a && y!=b) && b>=42) {}
-			gfxDrawRectangle(c,d,a,b);
-            break;
-        default :
-            break;
+        }
+        break;
+    case 'l':
+        while (touchAvailable(&a2, &b2) == enBooleanTrue)
+        {
+            if (b2 >= 45)
+            {
+                a1 = a2;
+                b1 = b2;
+            }
+        }
+        if ((i != a1 || j != b1) && b1 >= 45)
+        {
+            gfxDrawLine(i, j, a1, b1);
+        }
+        break;
+    case 'c':
+        while (touchAvailable(&a2, &b2) == enBooleanTrue)
+        {
+            if (b2 >= 45)
+            {
+                a1 = a2;
+                b1 = b2;
+            }
+        }
+        if ((i != a1 || j != b1) && b1 >= 45)
+        {
+            PFdword radius, tmp = ((i - a1) * (i - a1)) + ((j - b1) * (j - b1));
+            radius = sqroot(tmp);
+            // if (i - radius >= 0 && i + radius >= 249 && j - radius >= 42 && j + radius <= 320) {}
+            if (j - radius >= 45)
+            {
+                gfxDrawCircle(i, j, radius);
+            }
+        }
+
+        break;
+    case 'r':
+        while (touchAvailable(&a2, &b2) == enBooleanTrue)
+        {
+            if (b2 >= 45)
+            {
+                a1 = a2;
+                b1 = b2;
+            }
+        }
+        if ((i != a1 && j != b1) && b1 >= 45)
+        {
+            gfxDrawRectangle(i, j, a1, b1);
+        }
+        break;
+    default:
+        break;
     }
 }
